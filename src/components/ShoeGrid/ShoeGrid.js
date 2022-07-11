2305a78b689272378b70b733874964dc0fbd78c6
Asheer Rizvi <asheer.vil@gmail.com>
2022-07-11 16:31:16 +0530

    Grid layout

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeWrapper key={shoe.slug}>
+          <ShoeCard {...shoe} />
+        </ShoeWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 32px;
+`;
+
+const ShoeWrapper = styled.div`
+  min-width: 275px;
+  flex: 1;
+  /* OR */
+  /* flex: 1 0 275px; */
+`;
 
 export default ShoeGrid;
